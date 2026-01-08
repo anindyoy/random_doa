e19da5dfa9685a8fb6e849b8fd00967a4dbc60b6
anindyo <anindyo.yudhistiro@gmail.com>
2026-01-08 14:11:09 +0700

    penyesuaian skema table resource doa.

--- app/Filament/Resources/Doas/Tables/DoasTable.php
+++ app/Filament/Resources/Doas/Tables/DoasTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Doas\Tables;
 use Filament\Tables\Table;
 use Filament\Actions\EditAction;
 use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\IconColumn;
 use Filament\Tables\Columns\TextColumn;
@@ -15,18 +16,27 @@ class DoasTable
     public static function configure(Table $table): Table
     {
         return $table
+            ->modifyQueryUsing(
+                fn($query) => auth()->user()->is_admin
+                    ? $query
+                    : $query->orWhere('user_id', auth()->id())
+            )
             ->columns([
                 TextColumn::make('judul')
                     ->searchable(),
-                ImageColumn::make('gambar'),
+                ImageColumn::make('gambar')
+                    ->disk('public')
+                    ->visibility('public'),
                 TextColumn::make('sumber_desain')
                     ->searchable(),
                 IconColumn::make('untuk_pribadi')
                     ->boolean(),
                 TextColumn::make('user.name')
-                    ->searchable(),
+                    ->searchable()
+                    ->visible(auth()->user()->is_admin),
                 IconColumn::make('visibility')
                     ->boolean(),
+                TextColumn::make('tags.nama'),
                 TextColumn::make('ajuan')
                     ->searchable(),
                 TextColumn::make('created_at')
@@ -42,7 +52,16 @@ class DoasTable
                 //
             ])
             ->recordActions([
-                EditAction::make(),
+                EditAction::make()->hiddenLabel()
+                    ->visible(
+                        fn($record) => auth()->user()->is_admin
+                            || $record->user_id === auth()->id()
+                    ),
+                DeleteAction::make()->hiddenLabel()
+                    ->visible(
+                        fn($record) => auth()->user()->is_admin
+                            || $record->user_id === auth()->id()
+                    ),
             ])
             ->defaultSort('id', 'desc')
             ->toolbarActions([
